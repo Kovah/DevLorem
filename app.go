3e672273ba234ce67aa77bfe41025bf040456646
Kovah <mail@kovah.de>
2020-07-07 23:42:26 +0200

    Add conditionally hidden or shown paragraph tags for quotes

--- app.go
+++ app.go
@@ -13,8 +13,9 @@ type Sources struct {
 }
 
 type Output struct {
-	Source     string
-	Paragraphs string
+	Source          string
+	Paragraphs      string
+	ShowsParagraphs bool
 }
 
 func check(e error) {
@@ -35,24 +36,44 @@ func getSources() Sources {
 	return sources
 }
 
+func getRandomContent() (string, string) {
+	sources := getSources()
+	source := sources.Sources[rand.Intn(len(sources.Sources))]
+
+	content, err := ioutil.ReadFile("lorem/" + source)
+	check(err)
+
+	parsedContent := string(content)
+
+	return source, parsedContent
+}
+
 func main() {
 
 	tmpl := template.Must(template.ParseFiles("template.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		sources := getSources()
-		source := sources.Sources[rand.Intn(len(sources.Sources))]
+		source, content := getRandomContent()
 
-		content, err := ioutil.ReadFile("lorem/" + source)
-		check(err)
-
-		parsedContent := string(content)
-
-		parsedContent = strings.ReplaceAll(parsedContent, "<p>", "")
-		parsedContent = strings.ReplaceAll(parsedContent, "</p>", "")
+		content = strings.ReplaceAll(content, "<p>", "")
+		content = strings.ReplaceAll(content, "</p>", "")
 
 		output := Output{
-			Source:     strings.TrimSuffix(source, ".txt"),
-			Paragraphs: parsedContent,
+			Source:          strings.TrimSuffix(source, ".txt"),
+			Paragraphs:      content,
+			ShowsParagraphs: false,
+		}
+
+		tmpl.Execute(w, output)
+	})
+
+	http.HandleFunc("/p", func(w http.ResponseWriter, r *http.Request) {
+		source, content := getRandomContent()
+
+		output := Output{
+			Source:          strings.TrimSuffix(source, ".txt"),
+			Paragraphs:      content,
+			ShowsParagraphs: true,
 		}
 
 		tmpl.Execute(w, output)
