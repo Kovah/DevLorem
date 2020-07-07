5c0e0e6bf4f41580495b6e521b42fe9e87bf4801
Kovah <mail@kovah.de>
2020-07-07 23:18:09 +0200

    Implement very basic website generator for random paragraphs

--- app.go
+++ app.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"strings"
+)
+
+type Sources struct {
+	Sources []string
+}
+
+type Output struct {
+	Source     string
+	Paragraphs string
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func getSources() Sources {
+	sourceDir, err := ioutil.ReadDir("lorem")
+	check(err)
+
+	sources := Sources{}
+	for _, entry := range sourceDir {
+		sources.Sources = append(sources.Sources, entry.Name())
+	}
+
+	return sources
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+
+	tmpl := template.Must(template.ParseFiles("template.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		sources := getSources()
+		source := sources.Sources[rand.Intn(len(sources.Sources))]
+
+		content, err := ioutil.ReadFile("lorem/" + source)
+		check(err)
+
+		parsedContent := string(content)
+
+		parsedContent = strings.ReplaceAll(parsedContent, "<p>", "")
+		parsedContent = strings.ReplaceAll(parsedContent, "</p>", "")
+
+		output := Output{
+			Source:     strings.TrimSuffix(source, ".txt"),
+			Paragraphs: parsedContent,
+		}
+
+		tmpl.Execute(w, output)
+	})
+
+	http.ListenAndServe(":80", nil)
 }
